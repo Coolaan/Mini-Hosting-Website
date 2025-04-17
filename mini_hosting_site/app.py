44ffd1391d367947c1af2d6efc59ef73873c77cb
Aanshi <aanshirawal9@gmail.com>
2025-04-18 00:37:45 +0530

    Update app.py

--- mini_hosting_site/app.py
+++ mini_hosting_site/app.py
@@ -42,5 +42,8 @@ def serve_user_files(username, filename):
     user_path = os.path.join(app.config['UPLOAD_FOLDER'], username)
     return send_from_directory(user_path, filename)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host='0.0.0.0', port=port, debug=True)
+
